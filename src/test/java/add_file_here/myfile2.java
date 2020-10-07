cc9a74e330553e87f479e496d9f5d4febf566286
muhsintamturk <muhsintamturk@gmail.com>
2020-10-07 17:11:18 +0100

    i added two files

---
+++ src/test/java/add_file_here/myfile2.java
@@ -0,0 +1,9 @@
+package add_file_here;
+
+public class myfile2 {
+    public static void main(String[] args) {
+        System.out.println("this is my second file");
+
+        //Do not make your harder than I teach
+    }
+}
