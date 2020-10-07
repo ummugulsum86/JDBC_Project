985266228821da082e5cf49bda8ca18889300150
Omer Ozdil <omerozdil54@gmail.com>
2020-10-07 21:33:11 +0100

    Fourth commit

--- src/test/java/add_file_here/NewBranch2File.java
+++ src/test/java/add_file_here/NewBranch2File.java
@@ -4,5 +4,7 @@ public class NewBranch2File {
     public static void main(String[] args) {
         System.out.println("This is NewBranch2");
         System.out.println("Onew more commit");
+        System.out.println("Third Commit");
+        System.out.println("Fourth Commit");
     }
 }
