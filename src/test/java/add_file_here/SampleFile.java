2feaa703cc7a6f822621cbf3e273ea6ccf8defe7
Cihat Turan <jack.turan@live.com>
2020-10-07 17:09:07 +0100

    New Project

--- src/test/java/add_file_here/SampleFile.java
+++ src/test/java/add_file_here/SampleFile.java
@@ -5,6 +5,7 @@ public class SampleFile {
     public static void main(String[] args) {
 
         System.out.println("Awesome stuff here");
+        System.out.println("Even better stuff after");
 
     }
 
