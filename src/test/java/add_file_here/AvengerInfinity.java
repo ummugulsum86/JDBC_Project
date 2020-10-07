cafddcc0a45a12cc7cfcc141ca9af354fe891897
Doolatkan <zdoolatkan@seznam.cz>
2020-10-07 22:30:32 +0200

    this is my second awesome commit

--- src/test/java/add_file_here/AvengerInfinity.java
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -4,5 +4,7 @@ public class AvengerInfinity {
     public static void main(String[] args) {
 
         //add four changes and commit with 4 separate commit messages
+
+        //I will add one more line to this awesomr class :)
     }
 }
