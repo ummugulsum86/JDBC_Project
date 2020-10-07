2fcf706b6a5e3126fae59e5c27a541344def546d
CorinaSirbu111 <nastasa_corina@yahoo.com>
2020-10-07 20:45:27 +0000

    More commited

--- src/test/java/add_file_here/Changes.java
+++ src/test/java/add_file_here/Changes.java
@@ -5,4 +5,6 @@ public class Changes {
     // I am trying to fallow some steps
 
     // I have added a new branch called practice1 from intellij
+    // We will add more changes
+    // so lets see what happen
 }
