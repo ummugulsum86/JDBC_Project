c6311580289d9a24bf3d33f78a4b0c6d78dba848
Omer Ozdil <omerozdil54@gmail.com>
2020-10-07 20:47:09 +0100

    Added more stuff for new Branch

--- src/test/java/add_file_here/newBranchFile.java
+++ src/test/java/add_file_here/newBranchFile.java
@@ -3,5 +3,6 @@ package add_file_here;
 public class newBranchFile {
     public static void main(String[] args) {
         System.out.println("This is for New Branch");
+        System.out.println("This is another one");
     }
 }
