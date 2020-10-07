c4a99413338c36090ee222791c92295c598ed1a4
muhsintamturk <muhsintamturk@gmail.com>
2020-10-07 17:34:18 +0100

    i added two files

--- src/test/java/add_file_here/myfile1.java
+++ src/test/java/add_file_here/myfile1.java
@@ -3,5 +3,13 @@ package add_file_here;
 public class myfile1 {
     public static void main(String[] args) {
         System.out.println("THIS IS MY FIRST FILE");
+
+        "You will face many defeats in life, but never let yourself be defeated." -Maya Angelou
+        "The greatest glory in living lies not in never falling, but in rising every time we fall." -Nelson Mandela
+        "In the end, it's not the years in your life that count. It's the life in your years." -Abraham Lincoln
+        "Never let the fear of striking out keep you from playing the game." -Babe Ruth
+        "Life is either a daring adventure or nothing at all." -Helen Keller
+        "Many of life's failures are people who did not realize how close they were to success when they gave up." -Thomas A. Edison
+        "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose." -Dr. Seuss
     }
 }
