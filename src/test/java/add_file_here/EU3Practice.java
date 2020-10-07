42e8a600f5afe48b9216ca8ac7461a13764c33fc
Tunc Celik <celiktunc44@gmail.com>
2020-10-07 20:45:34 +0100

    First Branch work intellij

--- src/test/java/add_file_here/EU3Practice.java
+++ src/test/java/add_file_here/EU3Practice.java
@@ -3,5 +3,7 @@ package add_file_here;
 public class EU3Practice {
     public static void main(String[] args) {
         System.out.println("Thanks");
+
+        //branch work without affecting master
     }
 }
