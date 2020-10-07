c6e6079c0d14b062538bf44c2e42c13eaaae2dc3
Sergio4334 <SERCANAYTR3444@gmail.com>
2020-10-07 17:58:56 +0200

    New ideas

---
+++ src/test/java/add_file_here/SercanAyLetsMakeChanges.java
@@ -0,0 +1,8 @@
+package add_file_here;
+
+public class SercanAyLetsMakeChanges {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Spain,don't miss any class of Akbar");
+    }
+}
