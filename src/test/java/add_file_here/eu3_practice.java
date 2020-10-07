30b0e4ce2e45d01b138cb4a31e96a707b046716a
EbubekirCiliv <72078134+EbubekirCiliv@users.noreply.github.com>
2020-10-07 18:27:33 +0200

    i added something new eu3 practice

--- src/test/java/add_file_here/eu3_practice.java
+++ src/test/java/add_file_here/eu3_practice.java
@@ -4,7 +4,8 @@ public class eu3_practice {
 
     public static void main(String[] args) {
         System.out.println("EU3 IS OPENING PULL REQUEST");
-
+     // hi every one
+     // I added new things
 
     }
 }
