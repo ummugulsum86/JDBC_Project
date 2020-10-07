47daed30fdca65a17ba638948b46419ca1cb1c28
muhsintamturk <muhsintamturk@gmail.com>
2020-10-07 17:35:39 +0100

    i added two files

--- src/test/java/add_file_here/myfile2.java
+++ src/test/java/add_file_here/myfile2.java
@@ -4,6 +4,12 @@ public class myfile2 {
     public static void main(String[] args) {
         System.out.println("this is my second file");
 
-        //Do not make your harder than I teach
-    }
+//        "Success is not final; failure is not fatal: It is the courage to continue that counts." -Winston S. Churchill
+//        "Success usually comes to those who are too busy to be looking for it." -Henry David Thoreau
+//        "The way to get started is to quit talking and begin doing." -Walt Disney
+//        "If you really look closely, most overnight successes took a long time." -Steve Jobs
+//        "The secret of success is to do the common thing uncommonly well." -John D. Rockefeller Jr.
+//        "I find that the harder I work, the more luck I seem to have." -Thomas Jefferson
+//        "The real test is not whether you avoid this failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere." -Barack Obama
+//    }
 }
