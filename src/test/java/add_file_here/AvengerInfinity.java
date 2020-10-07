842a49ce73773de02738a849df5455fdcc2b2dfe
Doolatkan <zdoolatkan@seznam.cz>
2020-10-07 22:31:45 +0200

    Added one more awesome stuff to my awesome class :)

--- src/test/java/add_file_here/AvengerInfinity.java
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -8,5 +8,7 @@ public class AvengerInfinity {
         //I will add one more line to this awesomr class :)
 
         //I like write code in intellij
+
+        //I dont what I can add here one more
     }
 }
