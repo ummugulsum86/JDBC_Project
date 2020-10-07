f4ff3f3c7d0e5595bc03b3a41e8e4581d0f7cd01
Joomart7878 <65704743+Joomart7878@users.noreply.github.com>
2020-10-07 22:27:56 +0600

    Practice adding

--- src/test/java/add_file_here/ChangedClass.java
+++ src/test/java/add_file_here/ChangedClass.java
@@ -4,5 +4,16 @@ public class ChangedClass {
 
     public static void main(String[] args) {
         System.out.println("Here is my new adding file");
+        //commit your change
+        // with nice commit message
+        //push it back to the github
+        //observe the change
+        //from the your github repo
+            //click on compare
+        //create a pull request (PR)
+            //to ask original repo owner
+            //to merge your change
+
+
     }
 }
