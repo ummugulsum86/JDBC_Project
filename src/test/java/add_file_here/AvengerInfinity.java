b83a162b664497eb3ea869e006d6c49c960368e3
muhsintamturk <muhsintamturk@gmail.com>
2020-10-07 21:31:24 +0100

    added hulk and thor

--- src/test/java/add_file_here/AvengerInfinity.java
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -5,5 +5,7 @@ public class AvengerInfinity {
     public static void main(String[] args) {
         //add or changes and commit with 4 seperate commit messages
         //Iron man
+
+        //hulk and thor
     }
 }
