30dd9b5a1d19b595c5464ab1c86a8abb867870a9
Doolatkan <zdoolatkan@seznam.cz>
2020-10-07 18:25:24 +0200

    added new java file

---
+++ src/test/java/add_file_here/MyAwesomeFile.java
@@ -0,0 +1,10 @@
+package add_file_here;
+
+
+public class MyAwesomeFile {
+
+    public static void main(String[] args) {
+        System.out.println("My awesome java code will be here soon");
+    }
+
+}
