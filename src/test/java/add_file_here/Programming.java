00a15f586c68d904c738afd7c07db3bac8dfddee
Melike24 <nurullah24yunus@gmail.com>
2020-10-07 21:46:46 +0200

    Added some lines to describe what we are doing in melike branch

--- src/test/java/add_file_here/Programming.java
+++ src/test/java/add_file_here/Programming.java
@@ -3,5 +3,8 @@ package add_file_here;
 public class Programming {
     public static void main(String[] args) {
         System.out.println("Say Hi to new life :)");
+
+        //we have added one new branch called melike
+        //to work on user melike  without affecting master
     }
 }
