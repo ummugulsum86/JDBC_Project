77ac86c16762e3069b495b6104f3d0bf031bb36c
GeorgeLondon1 <radac.george@yahoo.com>
2020-10-07 20:46:11 +0100

    practice creating a new branch from IntelliJ 2

--- src/test/java/add_file_here/AkbarClassIsGreat.java
+++ src/test/java/add_file_here/AkbarClassIsGreat.java
@@ -6,5 +6,5 @@ public class AkbarClassIsGreat {
     //we have added one new branch called us123
     //to work on user story 123 without affecting master
 
-
+    //adding more changes here so I can have more history
 }
