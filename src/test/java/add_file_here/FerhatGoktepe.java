23e363ac1efb28a5118a88ce56781c84928b1b51
ferhatgoktepe <ferhatgoktepe@hotmail.com>
2020-10-07 21:45:30 +0200

    Added some lines to explain Branching.

--- src/test/java/add_file_here/FerhatGoktepe.java
+++ src/test/java/add_file_here/FerhatGoktepe.java
@@ -3,4 +3,6 @@ package add_file_here;
 public class FerhatGoktepe {
 
     //Can you accept my pull request please:
+    // New branch called Branch_A was added.
+    // We now are working on IntelliJ Environment.
 }
