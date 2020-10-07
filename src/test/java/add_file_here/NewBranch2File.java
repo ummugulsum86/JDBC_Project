67100cc839023858e9ef1fc1ce6c778b37f68fd8
Omer Ozdil <omerozdil54@gmail.com>
2020-10-07 21:30:17 +0100

    second commit

--- src/test/java/add_file_here/NewBranch2File.java
+++ src/test/java/add_file_here/NewBranch2File.java
@@ -3,5 +3,6 @@ package add_file_here;
 public class NewBranch2File {
     public static void main(String[] args) {
         System.out.println("This is NewBranch2");
+        System.out.println("Onew more commit");
     }
 }
