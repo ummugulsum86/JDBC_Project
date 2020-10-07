715cfb50bb3f22c2c0c79a6e88a783094e3da509
ferhatgoktepe <ferhatgoktepe@hotmail.com>
2020-10-07 21:46:26 +0200

    Added some more lines.

--- src/test/java/add_file_here/FerhatGoktepe.java
+++ src/test/java/add_file_here/FerhatGoktepe.java
@@ -5,4 +5,6 @@ public class FerhatGoktepe {
     //Can you accept my pull request please:
     // New branch called Branch_A was added.
     // We now are working on IntelliJ Environment.
+
+    // Now adding more changes to have more history.
 }
