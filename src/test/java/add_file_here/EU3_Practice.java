d3ebe97d3c7d81cf186da245b3194faff7f7cd3a
Ilyas Ulgur <ulgur@yahoo.com>
2020-10-07 18:27:54 +0200

    First change on this project

---
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -0,0 +1,22 @@
+package add_file_here;
+
+public class EU3_Practice {
+
+    public static void main(String[] args) {
+        System.out.println("EU3 is opening pull request");
+
+        System.out.println("This course seems to be in the correct speed!");
+
+        //commit your change
+        //with nice commit message
+        //push it back to the github.
+        //observe the change
+        //from the your GitHub repo
+        //click on compare
+        //create a pull request (PR)
+                //to ask original repo owner
+                //to merge your change
+
+
+    }
+}
