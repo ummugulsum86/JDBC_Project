edfc4a24e0a28f7766b4008df9a0127e2fc85d17
Tunc Celik <celiktunc44@gmail.com>
2020-10-07 21:31:20 +0100

    branchworkClassChanges22222

--- src/test/java/add_file_here/BranchWork.java
+++ src/test/java/add_file_here/BranchWork.java
@@ -5,6 +5,7 @@ public class BranchWork {
     public static void main(String[] args) {
 
         //add four changes and commit with 4 separate
+        //I am already tried because of this lesson
         }
 
 
