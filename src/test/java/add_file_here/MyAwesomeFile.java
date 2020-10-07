8817fbf8200416e889a3207de501873f21f2a718
Doolatkan <zdoolatkan@seznam.cz>
2020-10-07 21:45:42 +0200

    Added some awesome line to describe what we are doing in us123

--- src/test/java/add_file_here/MyAwesomeFile.java
+++ src/test/java/add_file_here/MyAwesomeFile.java
@@ -5,6 +5,8 @@ public class MyAwesomeFile {
 
     public static void main(String[] args) {
         System.out.println("My awesome java code will be here soon");
+        //we have added one new branch called us123
+        //to work on user story 123 without affecting master
     }
 
 }
