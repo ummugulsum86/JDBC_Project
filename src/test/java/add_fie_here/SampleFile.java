eb3aa3a7c063c89791b9104beb600cf5542f84d4
Zamira2689 <salomovazamira.89@gmail.com>
2020-10-07 17:25:46 +0100

    changes commited

--- src/test/java/add_fie_here/SampleFile.java
+++ src/test/java/add_fie_here/SampleFile.java
@@ -3,6 +3,7 @@ package add_fie_here;
 public class SampleFile {
     public static void main(String[] args) {
         System.out.println("EU3 is opening pull request");
-        
+        //commit change
+        //push to github
     }
 }
