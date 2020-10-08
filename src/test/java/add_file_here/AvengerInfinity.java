d7e1f575bc8f3de98bd4879216e24c1efad852c1
Joomart7878 <65704743+Joomart7878@users.noreply.github.com>
2020-10-08 11:15:56 +0600

    Added Thor

--- src/test/java/add_file_here/AvengerInfinity.java
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -4,6 +4,7 @@ public class AvengerInfinity {
     public static void main(String[] args) {
         //add four changes and commit with 4 separate commit changes
         // adding more changes and adding Iron man
+        //CapAmerica, Iron man, Thor
 
     }
 }
