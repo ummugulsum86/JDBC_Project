adc9a5a15d269e5ed29ecb0ab9014e9bdd1213b2
EU3 Batch <yavuzyesilyurt3@gmail.com>
2020-10-13 20:22:20 +0100

    Avenf-ger fan here

---
+++ src/test/java/add_fie_here/Avenger.java
@@ -0,0 +1,10 @@
+package add_fie_here;
+
+public class Avenger {
+    public static void main(String[] args) {
+        System.out.println("create new branch and merge into master");
+
+        //add four changes and commit with separete commit messages
+
+    }
+}
