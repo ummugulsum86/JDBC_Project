207ccfd3b2866d594ff0af444ddbcae2a8df2b11
Ummgulsum Koc <umgk86@gmail.com>
2020-10-15 14:21:19 +0200

    update git ignore and added sample file

---
+++ src/test/java/add_fie_here/EU3_Practice.java
@@ -0,0 +1,24 @@
+package add_fie_here;
+
+public class EU3_Practice {
+    public static void main(String[] args) {
+        System.out.println("EU3 is opening pull request");
+        //commit your changes
+        //with nice commit message
+        //push it back to the github
+        //observe the change
+        //from the your Github repo
+        //click on compare
+        //create a pill request (PR)
+        //to asl original repo owner
+        //to merge your change
+
+
+
+
+
+
+    }
+
+
+}
